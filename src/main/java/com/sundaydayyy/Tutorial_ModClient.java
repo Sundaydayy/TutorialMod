05a9d9f7785c513c361e26c906d6b0a2d0a7a58a
Sundaydayy <abc1220374650@126.com>
2023-10-09 02:22:37 +0800

    1

---
+++ src/main/java/com/sundaydayyy/Tutorial_ModClient.java
@@ -0,0 +1,10 @@
+package com.sundaydayyy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Tutorial_ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
